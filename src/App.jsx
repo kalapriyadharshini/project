0dcbc846ebec3cbccba897a36ccb20b6ea261258
kalapriyadharshini <131987785+kalapriyadharshini@users.noreply.github.com>
2025-07-10 12:18:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import CustomNavbar from './components/CustomNavbar'
 import Home from './Pages/Home'
+
 function App() {
   const [count, setCount] = useState(0)
   return (
