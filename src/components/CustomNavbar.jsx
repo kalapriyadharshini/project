8fe142937c0e1c439061a0d590d8fbc548be743f
kalapriyadharshini <131987785+kalapriyadharshini@users.noreply.github.com>
2025-07-14 09:12:15 +0530

    changes

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -4,7 +4,7 @@ import { Navbar, Nav, Container, Button, Form, FormControl, Offcanvas } from "re
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { FaBars, FaSearch, FaHeart, FaUser, FaShoppingCart, FaHome } from "react-icons/fa";
 import './CustomNavbar.css';
-import SidebarMenu from './SidebarMenu'; 
+// import SidebarMenu from './SidebarMenu'; 
 
 const CustomNavbar = () => {
   const navigate = useNavigate();
@@ -147,7 +147,7 @@ const CustomNavbar = () => {
       
 
 {/* Inside return: below mobile nav */}
-<Offcanvas
+{/* <Offcanvas
   show={showSidebar}
   onHide={() => setShowSidebar(false)}
   scroll
@@ -161,7 +161,7 @@ const CustomNavbar = () => {
   <Offcanvas.Body>
     <SidebarMenu onClose={() => setShowSidebar(false)} />
   </Offcanvas.Body>
-</Offcanvas>
+</Offcanvas> */}
 
     </>
   );
